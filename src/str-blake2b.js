c8f94e1b861be8f7fb1fa9656d3971492c577b1f
Simon.Z <zhanghan.simon@gmail.com>
2022-10-24 14:58:22 -0700

    add support for hex/str blake2b

--- src/str-blake2b.js
+++ src/str-blake2b.js
@@ -3,11 +3,10 @@
 var blake2b = require('blake2b');
 
 if (process.argv.length < 3) {
-  console.log(`[Usage] b2b 'hex'`);
+  console.log(`[Usage] str-blake2 'str'`);
   process.exit(-1);
 }
 
-const text = process.argv[2].replace('0x', '');
-const input = Buffer.from(text, 'hex');
+const input = Buffer.from(process.argv[2], 'binary');
 const digest = blake2b(32).update(input).digest('hex');
 console.log(digest);
